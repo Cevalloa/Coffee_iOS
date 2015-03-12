53de40b293a8f0db9048121a1d94f554d3e992a4
Alex Cevallos <cevalloa@gmail.com>
2015-03-12 17:14:55 -0400

    Initial Commit

---
+++ Coffee/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Coffee
+//
+//  Created by Alex Cevallos on 3/12/15.
+//  Copyright (c) 2015 AlexCevallos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
